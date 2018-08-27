347c375b007ddbd547d500348fa7887703421499
Tom Haskins <tom.s.haskins@gmail.com>
2018-08-27 21:49:24 +0100

    add: netflify identity scripts

---
+++ front/src/js/components/netlify.js
@@ -0,0 +1,12 @@
+import Ready from 'ready'
+Ready(()=> {
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+})
--- front/src/js/main.js
+++ front/src/js/main.js
@@ -2,3 +2,4 @@ import "./components/data-js";
 import "./components/nav";
 import "./components/forms";
 import "./components/slider";
+import "./components/netlify";
